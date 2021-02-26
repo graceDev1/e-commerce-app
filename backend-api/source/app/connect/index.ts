0db06da2717427976485c9939a15c63ecd2dc332
graceDev1 <grace.devolop@gmail.com>
2021-02-26 17:24:45 +0200

    crud operation of the product 1

--- backend-api/source/app/connect/index.ts
+++ backend-api/source/app/connect/index.ts
@@ -1,9 +1,5 @@
 import mongoose from 'mongoose';
-// import config from 'config';
 
-// const uri : string = config.get('uri');
-
-// console.log(uri);
 mongoose.connect('mongodb://127.0.0.1:27017/SaraShop', {
     useUnifiedTopology: true, 
     useNewUrlParser:true,
